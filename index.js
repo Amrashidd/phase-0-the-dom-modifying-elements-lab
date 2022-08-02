376cb4455c14dcb0ca5fbedcf160016af9df835c
ahmed rashid <ahmeddmrashid@gmail.com>
2022-08-02 23:46:06 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector("main")
+main.remove();
+
+var newHeader = document.createElement('h1'); 
+newHeader.setAttribute("id", "victory");
+
+newHeader. innerHTML = "Ahmed is the champion"
